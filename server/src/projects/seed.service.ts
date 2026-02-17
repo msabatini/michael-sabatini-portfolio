f41757b377989ccd0962782b7f745aa096730e02
msabatini <michaelsabatinidesign@gmail.com>
2026-02-16 19:12:47 -0500

    Remove deleted Service Course bizcard assets and bump seeder to v99

--- server/src/projects/seed.service.ts
+++ server/src/projects/seed.service.ts
@@ -14,8 +14,8 @@ export class SeedService implements OnModuleInit {
   ) {}
 
   async onModuleInit() {
-    const SEEDER_VERSION = 'v98';
-    this.logger.log(`FORCE SEED: v98`);
+    const SEEDER_VERSION = 'v99';
+    this.logger.log(`FORCE SEED: v99`);
     this.logger.log(`Starting project seeding process [${SEEDER_VERSION}]...`);
     
     try {
