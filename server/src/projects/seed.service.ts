9d8e0fece3d8766db80f1c5444e289303e9ef282
msabatini <michaelsabatinidesign@gmail.com>
2026-02-16 21:50:20 -0500

    Update profile photo and seed data

--- server/src/projects/seed.service.ts
+++ server/src/projects/seed.service.ts
@@ -14,8 +14,8 @@ export class SeedService implements OnModuleInit {
   ) {}
 
   async onModuleInit() {
-    const SEEDER_VERSION = 'v102';
-    this.logger.log(`FORCE SEED: v102`);
+    const SEEDER_VERSION = 'v103';
+    this.logger.log(`FORCE SEED: v103`);
     this.logger.log(`Starting project seeding process [${SEEDER_VERSION}]...`);
     
     try {
